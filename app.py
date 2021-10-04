f8f4bd5d4637a1431338d0b0240774e11dcb0f52
J0hn1975 <johnwrafter@live.co.uk>
2021-10-04 16:25:22 +0000

    Create add_lyrics template

--- app.py
+++ app.py
@@ -106,6 +106,11 @@ def logout():
     return redirect(url_for("login"))
 
 
+@app.route("/add_lyrics")
+def add_lyrics():
+    return render_template("add_lyrics.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
