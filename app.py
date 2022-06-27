c7f561369c7074f77a2da1ee12f5511636ff5df0
J0hn1975 <johnwrafter@live.co.uk>
2022-06-27 11:21:02 +0000

    Changed appconfig for MONGO URI

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-import dns
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
@@ -15,7 +14,7 @@ app = Flask(__name__)
 
 app.secret_key = os.environ.get("SECRET_KEY")
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = "mongodb+srv://J0hn1975:N1k0nD750@myfirstcluster.0yzye.mongodb.net/lyric_finder?retryWrites=true&w=majority/"
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI") 
 mongo = PyMongo(app)
 
 
